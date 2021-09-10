7cb5dc696889a7c046b14e58efe9db82e384e761
Artur Calderon <artur.calderonart@gmail.com>
2021-09-10 19:22:23 -0400

    new commit

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Modal.css'
+
+function Modal(props) {
+  console.log(props.itemCart);
+  return (
+    <h1>teste</h1>
+    // <div className="modal ">
+    //   <div className="content-prod">
+    //     <div className="content-img">
+    //       <img src={itemCart.info.arquivoURL} alt="Foto do Produto" />
+    //     </div>
+    //     <div className="info-Content">
+    //       <h4 key={itemCart.id}>{itemCart.info.name}</h4>
+    //       <p>{itemCart.info.ingredientes}</p>
+    //       <b>R${itemCart.info.preco}</b>
+    //     </div>
+    //   </div>
+    // </div>
+  )
+}
+
+export default Modal
--- src/Components/ProductData.jsx
+++ src/Components/ProductData.jsx
@@ -1,40 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import './ProductData.css'
-
+import Modal from './Modal'
 
 export default function ProductData() {
   const [product, setProduct] = useState([])
   const [category, setCategory] = useState([])
-  const [cart , setCart] = useState([])
+  const [cart, setCart] = useState([])
+  const [isVisibleModal, setVisibleModal] = useState(false)
 
   useEffect(() => {
-
-    db.collection('categoria').onSnapshot((item) => {
-      setCategory(item.docs.map((val) => {
-
-        return { id: val.id, cat: val.data().categoryName }
-      }))
-
+    db.collection('categoria').onSnapshot(item => {
+      setCategory(
+        item.docs.map(val => {
+          return { id: val.id, cat: val.data().categoryName }
+        })
+      )
     })
   }, [])
 
   useEffect(() => {
-    db.collection('produtos').orderBy('categoria').onSnapshot((data) => {
-      setProduct(data.docs.map((item) => {
-        return { id: item.id, cat: item.data().categoria, info: item.data() }
-
-
-
-      }))
-    })
-
+    db.collection('produtos')
+      .orderBy('categoria')
+      .onSnapshot(data => {
+        setProduct(
+          data.docs.map(item => {
+            return {
+              id: item.id,
+              cat: item.data().categoria,
+              info: item.data()
+            }
+          })
+        )
+      })
   }, [])
 
   //Sempre que for usar evento de click, usar uma chamada para função no elemento react
   function toggleContent(e) {
     let elementChild = document.querySelector('#' + e.target.innerHTML)
-  
+
     if (elementChild.classList.contains('--selected')) {
       elementChild.classList.remove('--selected')
     } else {
@@ -42,102 +46,89 @@ export default function ProductData() {
     }
   }
 
-  function addToCart(item){
-    setCart([...cart,item])
-    console.log(cart);
+  function addToCart(item) {
+    setCart([...cart, item])
+    console.log(cart)
+  }
+
+  function renderCart() {
+    ;<>
+      {cart.map((item, idx) => {
+        return (
+          <div className="content-prod" key={idx}>
+            <div className="content-img">
+              <img src={item.info.arquivoURL} alt="Foto do Produto" />
+            </div>
+            <div className="info-Content">
+              <h4 key={item.id}>{item.info.name}</h4>
+              <p>{item.info.ingredientes}</p>
+              <b>R${item.info.preco}</b>
+              {/* <button onClick={()=> addToCart(item)}>Add To Cart</button> */}
+            </div>
+          </div>
+        )
+      })}
+    </>
+  }
+  function algo(item) {
+    <Modal itemCart={item}/>
+    addToCart(item)
+    setVisibleModal(true);
+    console.log(isVisibleModal);
     
   }
 
-  function abreTela(item){
-  //  let modal =  document.querySelector('.modal')
-  //   modal.style.display = 'block'
-   
-    
-    }
-    function renderCart(){
-      <>
-      {
-      cart.map((item ,idx) => {
-          return (
-            <div className="content-prod" key={idx}>
-              <div className="content-img">
-                <img src={item.info.arquivoURL} alt="Foto do Produto" />
-              </div>
-              <div className="info-Content">
-                <h4 key={item.id}>{item.info.name}</h4>
-                <p>{item.info.ingredientes}</p>
-                <b>R${item.info.preco}</b>
-                {/* <button onClick={()=> addToCart(item)}>Add To Cart</button> */}
-              </div>
-            </div>
-          )
-      })
-      }
-      </>
-    }
-
   return (
-    
     <div>
-      <div className="modal">  
-             
-       </div>
-
-
-
+      {isVisibleModal ? <Modal/> : null}
       <div className="accordion">
-
         <div className="accordion__item">
-
           <div className="accordion__header">
             <div className="accordion__title">
-              {
-                category.map((val) => {
-                  const cat = product.find((user) => user.cat === val.cat)
-                  if (cat) {
-                    return (
-                      <div>
-                        <h1 onClick={e => toggleContent(e)} key={val.id} className="title-prod">{val.cat}</h1>
-                        <div id={val.cat} className="accordion__content">
-                          {
-                            product.map((item ,idx) => {
-
-                              if (val.cat === item.info.categoria) {
-                                return (
-                                  <div className="content-prod" key={idx}>
-                                    <div className="content-img">
-                                      <img src={item.info.arquivoURL} alt="Foto do Produto" />
-                                    </div>
-                                    <div className="info-Content">
-                                      <h4 key={item.id}>{item.info.name}</h4>
-                                      <p>{item.info.ingredientes}</p>
-                                      <b>R${item.info.preco}</b>
-                                      <button onClick={()=> addToCart(item)}>Add To Cart</button>
-                                    </div>
-                                  </div>
-                                )
-                              }
-
-                            })
+              {category.map(val => {
+                const cat = product.find(user => user.cat === val.cat)
+                if (cat) {
+                  return (
+                    <div>
+                      <h1
+                        onClick={e => toggleContent(e)}
+                        key={val.id}
+                        className="title-prod"
+                      >
+                        {val.cat}
+                      </h1>
+                      <div id={val.cat} className="accordion__content">
+                        {product.map((item, idx) => {
+                          if (val.cat === item.info.categoria) {
+                            return (
+                              <div className="content-prod" key={idx}>
+                                <div className="content-img">
+                                  <img
+                                    src={item.info.arquivoURL}
+                                    alt="Foto do Produto"
+                                  />
+                                </div>
+                                <div className="info-Content">
+                                  <h4 key={item.id}>{item.info.name}</h4>
+                                  <p>{item.info.ingredientes}</p>
+                                  <b>R${item.info.preco}</b>
+                                  <button onClick={() => algo(item)}>
+                                    Add To Cart
+                                  </button>
+                                </div>
+                              </div>
+                            )
                           }
-                        </div>
-
+                        })}
                       </div>
-
-                    )
-
-                  }
-
-                })
-              }
+                    </div>
+                  )
+                }
+              })}
             </div>
           </div>
-
         </div>
-
       </div>
-
-
-    </div >
+    </div>
   )
 }
